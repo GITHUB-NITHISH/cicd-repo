6706a267527ca29591a67b39db01f52d3d946060
Nithish Kumar <sknithish0411@gmail.com>
2025-10-31 22:15:06 +0530

    cicdtest

---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class SampleTest extends BaseTest {
+
+    @Test
+    public void openAmazon_homepageTitle() {
+        driver.get("https://www.amazon.in");
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+        assertTrue(title.toLowerCase().contains("amazon"));
+    }
+}
